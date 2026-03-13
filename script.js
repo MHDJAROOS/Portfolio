84a1784f29975ccb4db25257864fc251ad500a0c
MHDJAROOS <mohammedjaroos4536@gmail.com>
2026-03-13 21:49:33 +0530

    sendemail

--- script.js
+++ script.js
@@ -7,4 +7,26 @@ var typed = new Typed(".text", {
     loop: true
 });
 
+//Email JS
 
+function sendEmail() {
+
+    let params = {
+        from_name: document.getElementById("Name").value,
+        email_id: document.getElementById("email").value,
+        message: document.getElementById("message").value
+    };
+
+    emailjs.send("service_mu5u49j", "template_8vdeo1c", params)
+    .then(function(response) {
+
+        alert("Email sent successfully!");
+
+    }, function(error) {
+
+        alert("Failed to send email!");
+        console.log(error);
+
+    });
+
+}
